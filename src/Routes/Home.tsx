114b1d58aa60bc8a0e20607dc099df3a0756939e
yen <l.yennie.j@gmail.com>
2023-01-23 02:42:25 +0900

    슬라이더 컴포넌트 분리하기

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -11,10 +11,10 @@ import {
 import React from "react";
 
 import styled from "styled-components";
-import { motion, AnimatePresence, wrap } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { makeImagePath } from "../utilis";
-import { useState } from "react";
 import { useNavigate, useMatch, PathMatch } from "react-router-dom";
+import Temp from "Components/Slider";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -38,6 +38,13 @@ const Banner = styled.div<{ bgPhoto: string }>`
   background-size: cover;
 `;
 
+const SliderWrapper = styled(motion.div)`
+  position: relative;
+  top: -100px;
+
+  border: 1px solid red;
+`;
+
 const Title = styled.h2`
   font-size: 68px;
   margin-bottom: 20px;
@@ -46,81 +53,6 @@ const Title = styled.h2`
 const Overview = styled.p`
   font-size: 18px;
   width: 50%;
-`;
-
-const Slider = styled(motion.div)`
-  position: relative;
-  top: -100px;
-`;
-
-const Row = styled(motion.div)`
-  padding: 0 40px;
-
-  display: grid;
-  gap: 5px;
-  grid-template-columns: repeat(6, 1fr);
-
-  position: absolute;
-  width: 100%;
-`;
-
-const IconBox = styled.div`
-  position: absolute;
-
-  height: 200px;
-  width: 40px;
-  background-color: rgba(0, 0, 0, 0.7);
-
-  font-size: 40px;
-  font-weight: 400;
-
-  z-index: 999999;
-`;
-
-const PrevButton = styled(IconBox)`
-  left: 0;
-`;
-
-const NextButt = styled(IconBox)`
-  right: 0;
-`;
-
-const Icon = styled(motion.div)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 100%;
-
-  cursor: pointer;
-`;
-
-const Box = styled(motion.div)<{ bgPhoto: string }>`
-  background-color: #fff;
-  background-size: cover;
-  background-image: url(${(props) => props.bgPhoto});
-  background-position: center center;
-  height: 200px;
-  cursor: pointer;
-  &:first-child {
-    transform-origin: center left;
-  }
-  &:last-child {
-    transform-origin: center right;
-  }
-`;
-
-const Info = styled(motion.div)`
-  padding: 20px;
-  background-color: ${(props) => props.theme.black.lighter};
-  opacity: 0;
-  position: absolute;
-  width: 100%;
-  bottom: 0;
-  h4 {
-    text-align: center;
-    font-size: 18px;
-  }
 `;
 
 const Overlay = styled(motion.div)`
@@ -167,54 +99,6 @@ const BigOverview = styled.p`
   color: ${(props) => props.theme.white.lighter};
 `;
 
-const rowVariants = {
-  hidden: (direction: number) => ({
-    x: direction > 0 ? window.outerWidth + 5 : -window.outerWidth - 5,
-  }),
-  visible: {
-    x: 0,
-  },
-  exit: (direction: number) => ({
-    x: direction < 0 ? window.outerWidth + 5 : -window.outerWidth - 5,
-  }),
-};
-
-const boxVariants = {
-  normal: {
-    scale: 1,
-  },
-  hover: {
-    scale: 1.3,
-    // y: -80,
-    transition: {
-      delay: 0.5,
-      duaration: 0.1,
-      type: "tween",
-    },
-  },
-};
-
-const infoVariants = {
-  hover: {
-    opacity: 1,
-    transition: {
-      delay: 0.5,
-      duaration: 0.1,
-      type: "tween",
-    },
-  },
-};
-
-const iconVariants = {
-  normal: {
-    scale: 1,
-  },
-  hover: {
-    scale: 1.3,
-  },
-};
-const offset = 6;
-
 const Home = () => {
   const navigate = useNavigate();
   const moviePathMatch: PathMatch<string> | null = useMatch("/movies/:id");
@@ -248,28 +132,6 @@ const Home = () => {
     getTrend
   );
 
-  const [index, setIndex] = useState(0);
-  const [hover, setHover] = useState(false);
-  const [[page, direction], setPage] = useState([0, 0]);
-
-  const increaseIndex = (newDirection: number) => {
-    if (data) {
-      const totalMovies = data.results.length - 1;
-      const maxIndex = Math.floor(totalMovies / offset) - 1;
-      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
-    }
-    setPage([page + newDirection, newDirection]);
-  };
-
-  const decreaseIndex = (newDirection: number) => {
-    if (data) {
-      const totalMovies = data.results.length - 1;
-      const maxIndex = Math.floor(totalMovies / offset) - 1;
-      setIndex((prev) => (prev === 0 ? maxIndex : prev - 1));
-    }
-    setPage([page + newDirection, newDirection]);
-  };
-
   // increase 와 decrease를 더 깔끔하게 쓸수 있게 하기
   // const paginate = (newDirection: number) => {
   //   setPage([page + newDirection, newDirection]);
@@ -301,63 +163,14 @@ const Home = () => {
               <Title>{data?.results[0].name}</Title>
               <Overview>{data?.results[0].overview}</Overview>
             </Banner>
-            <Slider
-              onHoverStart={() => {
-                setHover(true);
-              }}
-              onHoverEnd={() => {
-                setHover(false);
-              }}
-            >
-              <h1>오늘의 콘텐츠</h1>
-              {hover && (
+            <SliderWrapper>
+              {data && (
                 <>
-                  <PrevButton onClick={() => decreaseIndex(-1)}>
-                    <Icon variants={iconVariants} whileHover="hover">
-                      &lt;
-                    </Icon>
-                  </PrevButton>
-                  <NextButt onClick={() => increaseIndex(1)}>
-                    <Icon variants={iconVariants} whileHover="hover">
-                      &gt;
-                    </Icon>
-                  </NextButt>
+                  <Temp data={data} />
                 </>
               )}
-
-              <AnimatePresence initial={false} custom={direction}>
-                <Row
-                  variants={rowVariants}
-                  initial="hidden"
-                  animate="visible"
-                  exit="exit"
-                  transition={{ type: "tween", duration: 1 }}
-                  key={page}
-                  custom={direction}
-                >
-                  {data?.results
-                    .slice(1)
-                    .slice(offset * index, offset * index + offset)
-                    .map((movie) => (
-                      <Box
-                        layoutId={movie.id + ""}
-                        onClick={() => onBoxClicked(movie.id)}
-                        key={movie.id}
-                        variants={boxVariants}
-                        whileHover="hover"
-                        initial="normal"
-                        transition={{ type: "tween" }}
-                        bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                      >
-                        <Info variants={infoVariants}>
-                          {movie.name ? movie.name : movie.title}
-                        </Info>
-                      </Box>
-                    ))}
-                </Row>
-              </AnimatePresence>
-            </Slider>
-            <AnimatePresence>
+            </SliderWrapper>
+            {/* <AnimatePresence>
               {moviePathMatch ? (
                 <>
                   <Overlay
@@ -383,7 +196,7 @@ const Home = () => {
                   </BigMovie>
                 </>
               ) : null}
-            </AnimatePresence>
+            </AnimatePresence> */}
           </>
         )}
       </Wrapper>
