b7f1c10e1f40e69fe8aa27141d2b8782b45e2acc
yen <l.yennie.j@gmail.com>
2023-02-13 04:40:40 +0900

    react-hook-form basic

--- src/Components/Singup.tsx
+++ src/Components/Singup.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FieldError, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import bg from "../assets/netflixLoginbg.jpeg";
@@ -82,11 +83,13 @@ const Content = styled.div`
   }
 `;
 
-const Form = styled.form`
+// 클리하기 전에 첫 화면에서는 border없애고 싶음
+const Form = styled.form<{ errors?: FieldError }>`
   display: flex;
   flex-direction: column;
 
   padding: 0 20px;
+
   h3 {
     font-weight: 400;
     font-size: 18px;
@@ -96,7 +99,6 @@ const Form = styled.form`
   div {
     display: flex;
     width: 100%;
-
     :nth-child(1) {
       position: relative;
       height: 100%;
@@ -104,14 +106,18 @@ const Form = styled.form`
       input {
         color: white;
         background-color: rgba(22, 22, 22, 0.7);
-        border: 1px solid rgb(102, 198, 119);
+        border: ${(props) =>
+          props.errors ? "1px solid #e50914" : "1px solid #66c677"};
+
         border-radius: 4px;
         width: 100%;
-        font-size: 20px;
+        font-size: 25px;
         padding: 25px 20px 10px 20px;
         :focus {
           outline: 2px solid white;
           outline-offset: 2px;
+        }
+        :focus {
         }
       }
       label {
@@ -123,6 +129,7 @@ const Form = styled.form`
         font-weight: 400;
       }
     }
+
     button {
       width: 200px;
       margin-left: 10px;
@@ -137,15 +144,36 @@ const Form = styled.form`
         background-color: rgb(180, 0, 0);
       }
     }
+    :nth-child(3) {
+      padding-top: 8px;
+      svg {
+        fill: #e50914;
+      }
+      span {
+        font-size: 16px;
+        font-weight: 400;
+        color: #e50914;
+        line-height: 27px;
+        padding-left: 5px;
+      }
+    }
   }
 `;
 
-const temp = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  console.log("gg");
-};
-
+interface SingupForm {
+  email: string;
+}
 const Singup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SingupForm>();
+
+  const onValid = () => {
+    console.log("gg");
+  };
+
   return (
     <>
       <Backgroud bg={bg} />
@@ -165,7 +193,7 @@ const Singup = () => {
           <h1>영화와 시리즈를 무제한으로.</h1>
           <p>다양한 디바이스에서 시청하세요. 언제든 해지하실 수 있습니다.</p>
 
-          <Form onSubmit={temp}>
+          <Form onSubmit={handleSubmit(onValid)} errors={errors.email}>
             <h3>
               시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일
               주소를 입력하세요.
@@ -173,10 +201,34 @@ const Singup = () => {
             <div>
               <div>
                 <label>이메일 주소</label>
-                <input type="text" />
+                <input
+                  type="text"
+                  {...register("email", {
+                    required: "이메일 주소를 입력해주세요.",
+                    pattern: {
+                      value:
+                        /^([A-Za-z0-9]+){3}@([a-zA-Z0-9]+)\.([a-zA-Z]{2,5})$/,
+                      message: "정확한 이메일 주소를 입력하세요.",
+                    },
+                  })}
+                />
               </div>
-              {/* link로 변환할것 */}
               <button type="submit"> 시작하기 &gt;</button>
+            </div>
+            <div>
+              {errors.email && (
+                <>
+                  <svg
+                    width="25"
+                    height="25"
+                    viewBox="0 0 16 16"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M8,2.5c-3.03757,0 -5.5,2.46243 -5.5,5.5c0,3.0376 2.46243,5.5 5.5,5.5c3.0376,0 5.5,-2.4624 5.5,-5.5c0,-3.03757 -2.4624,-5.5 -5.5,-5.5zM15,8c0,3.866 -3.134,7 -7,7c-3.86599,0 -7,-3.134 -7,-7c0,-3.86599 3.13401,-7 7,-7c3.866,0 7,3.13401 7,7zM6.03033,4.96967l1.96967,1.96967l1.96967,-1.96967l1.06063,1.06066l-1.96964,1.96967l1.96964,1.96967l-1.06063,1.06063l-1.96967,-1.96964l-1.96967,1.96964l-1.06066,-1.06063l1.96967,-1.96967l-1.96967,-1.96967z" />
+                  </svg>
+                  <span>{errors.email.message}</span>
+                </>
+              )}
             </div>
           </Form>
         </Content>
